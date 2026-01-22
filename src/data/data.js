6184b6c89300583b365385a6d1fed308f642d931
Vraj Panchal <panchalvraj10@gmail.com>
2026-01-22 12:04:26 +0530

    changes Updated

--- src/data/data.js
+++ src/data/data.js
@@ -3,19 +3,19 @@ const shoes = [
     id: 1,
     name: "Nike",
     price: 3000,
-    img: "https://via.placeholder.com/100"
+    img: "#"
   },
   {
     id: 2,
     name: "Adidas",
     price: 2500,
-    img: "https://via.placeholder.com/100"
+    img: "#"
   },
   {
     id: 3,
     name: "Puma",
     price: 2800,
-    img: "https://via.placeholder.com/100"
+    img: "#"
   }
 ];
 
